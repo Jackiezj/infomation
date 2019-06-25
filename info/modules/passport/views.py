89339e4f0539c87cc264dd11e45ab3104a4f4cd3
jackiezz <jackiezz@163.com>
2019-06-26 01:12:09 +0800

    注册后端逻辑实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,13 +1,59 @@
 import random
 import re
+from datetime import datetime
 
-from flask import request, jsonify, current_app, make_response
+from flask import request, jsonify, current_app, make_response, session
 
-from info import redis_store, constants
+from info import redis_store, constants, db
 from info.libs.yuntongxun.sms import CCP
+from info.models import User
 from info.modules.passport import passport_blue
 from info.utils.captcha.captcha import captcha
 from info.utils.response_code import RET
+
+
+@passport_blue.route('/register', methods=['POST'])
+def register():
+    # 获取参数
+    params_dict = request.json
+    mobile = params_dict.get('mobile')
+    sms_code = params_dict.get('sms_code')
+    password = params_dict.get('password')
+
+    # 校验参数
+    if not all([mobile, sms_code, password]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不完整')
+    if not re.match(r'1[3-9]\d{9}', mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg='手机号格式不正确')
+    try:
+        real_sms_code = redis_store.get('sms_code_%s' % mobile)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据库查询错误')
+    if not real_sms_code:
+        return jsonify(errno=RET.PARAMERR, errmsg='短信验证码已过期')
+    if sms_code.upper() != real_sms_code.upper():
+        return jsonify(errno=RET.DATAERR, errmsg='短信验证码不正确')
+
+    # 业务处理
+    user = User()
+    user.mobile = mobile
+    user.password = password
+    user.nick_name = mobile
+    user.last_login = datetime.now()
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据库保存错误')
+
+    # 记录登录状态
+    session['user_id'] = user.id
+    session['mobile'] = user.mobile
+    session['nick_name'] = user.nick_name
+
+    return jsonify(errno=RET.OK, errmsg='注册成功')
 
 
 @passport_blue.route('/send_sms_codes', methods=['POST'])
@@ -36,6 +82,12 @@ def send_sms_codes():
     # 业务逻辑
     sms_code = "%06d" % random.randint(0, 999999)
     current_app.logger.debug("短信验证码为: %s" % sms_code)
+    try:
+        redis_store.set("sms_code_%s" % mobile, sms_code, 3600)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据库查询失败')
+
     result = CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES], "1")
 
     return jsonify(errno=RET.OK, errmsg='发送成功')
